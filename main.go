222a68a520bd6035a91267d53030f91467469528
Alvaro Saurin <alvaro.saurin@midokura.com>
2014-09-29 19:16:29 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/goraft/raft"
+	"github.com/inercia/divs/command"
+	"github.com/inercia/divs/server"
+	"github.com/voxelbrain/goptions"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var verbose bool
+var trace bool
+var debug bool
+var host string
+var port int
+var join string
+
+func main() {
+	log.SetFlags(0)
+
+	options := struct {
+		Host string `goptions:"-h, --host, description='Hostname'"`
+		Port int    `goptions:"-p, --port, description='Port'"`
+		Join string `goptions:"-j, --join, obligatory, description='host:port of leader to join'"`
+		Path string `goptions:"-d, --data, obligatory, description='data path directory'"`
+
+		// other
+		Timeout time.Duration `goptions:"-t, --timeout, description='Connection timeout in seconds'"`
+		Help    goptions.Help `goptions:"-h, --help, description='Show this help'"`
+		Verbose bool          `goptions:"-v, --verbose"`
+		Trace   bool          `goptions:"-t, --trace"`
+		Debug   bool          `goptions:"-d, --debug"`
+	}{ // Default values goes here
+		Timeout: 10 * time.Second,
+	}
+	goptions.ParseAndFail(&options)
+
+	flag.Parse()
+	if option.Verbose {
+		log.Print("Verbose logging enabled.")
+	}
+	if options.Trace {
+		raft.SetLogLevel(raft.Trace)
+		log.Print("Raft trace debugging enabled.")
+	} else if options.Debug {
+		raft.SetLogLevel(raft.Debug)
+		log.Print("Raft debugging enabled.")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	// Setup commands.
+	raft.RegisterCommand(&command.WriteCommand{})
+
+	// Set the data directory.
+	if err := os.MkdirAll(options.Path, 0744); err != nil {
+		log.Fatalf("Unable to create path: %v", err)
+	}
+
+	log.SetFlags(log.LstdFlags)
+	s := server.New(options.Path, options.Host, options.Port)
+	log.Fatal(s.ListenAndServe(join))
+}
