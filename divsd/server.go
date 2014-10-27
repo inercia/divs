96fcc6fbc46f2db8e6c94c70cf10610627fc63d2
Alvaro Saurin <alvaro.saurin@midokura.com>
2014-10-27 21:55:16 +0100

    Code cleanup

--- divsd/server.go
+++ divsd/server.go
@@ -20,6 +20,10 @@ type Server struct {
 
 // Creates a new server.
 func New(config *Config) (s *Server, err error) {
+	if config == nil {
+		return nil, fmt.Errorf("could not initialize the server")
+	}
+
 	// Initialize the device manager
 	devManager, err := NewDevManager(config)
 	if err != nil {
@@ -58,20 +62,17 @@ func (s *Server) ListenAndServe() error {
 	if err = s.nodesManager.Start(membersExternalAddr); err != nil {
 		log.Fatalf("Error when initialing peers manager: %s", err)
 	}
-	log.Debug("Is leader? %t", s.config.Raft.IsLeader)
-	if s.config.Raft.IsLeader {
-		log.Debug("... yes: we will try to connect to the leader")
-		s.nodesManager.WaitForNodes()
-	} else {
-		log.Debug("... no: we will not try to connect to anyone")
-	}
 
 	// and the devices manager
 	if err = s.devManager.Start(); err != nil {
 		log.Fatalf("Error when initialing tun/tap device manager: %s", err)
 	}
 
-	if !s.config.Raft.IsLeader {
+	log.Debug("Is leader? %t", s.config.Raft.IsLeader)
+	if s.config.Raft.IsLeader {
+		log.Debug("... yes: we will try to connect to the leader")
+		s.nodesManager.WaitForNodes()
+
 		// Join to leader if specified.
 		log.Info("Attempting to join leader:", s.config.Raft.Leader)
 
@@ -81,8 +82,14 @@ func (s *Server) ListenAndServe() error {
 		if err := s.raftServer.JoinLeader(membersExternalAddr.String(), s.config.Raft.Leader); err != nil {
 			log.Fatalf("Could not join %s: %v", s.config.Raft.Leader, err)
 		}
-	} else if err := s.raftServer.InitCluster(membersExternalAddr.String()); err != nil {
-		log.Fatalf("Could not initialize cluster: %v", err)
+	} else {
+		log.Debug("... no: we will not try to connect to anyone")
+		if err := s.raftServer.InitCluster(membersExternalAddr.String()); err != nil {
+			log.Fatalf("Could not initialize cluster: %v", err)
+		}
+
+		log.Debug("... yes: we will try to connect to the leader")
+		s.nodesManager.WaitForNodes()
 	}
 
 	log.Debug("Initializing Raft transport")
