6934e718541ffe608f7131b60424fb40039f31ea
Alvaro Saurin <alvaro.saurin@midokura.com>
2014-10-28 19:36:52 +0100

    Gems seems to be missing...

--- tools/ci/drone.io/tasks.d/15-fpm.sh
+++ tools/ci/drone.io/tasks.d/15-fpm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 echo ">>> Installing FPM..."
-sudo apt-get install ruby-dev gcc
+sudo apt-get install ruby ruby-dev gcc
+[ $? -eq 0 ] || (echo "ERROR: could not install packages" ; exit 1; )
+
 sudo gem install fpm
+[ $? -eq 0 ] ||  || (echo "ERROR: could not install FPM" ; exit 1; )
 
 exit 0
