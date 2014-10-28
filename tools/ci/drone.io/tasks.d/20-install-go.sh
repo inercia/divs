e6ad59a81cdc4cb49e5c2704b4428746dbabc33c
Alvaro Saurin <alvaro.saurin@midokura.com>
2014-10-28 17:38:11 +0100

    Forgot the pushd/popd

--- tools/ci/drone.io/tasks.d/20-install-go.sh
+++ tools/ci/drone.io/tasks.d/20-install-go.sh
@@ -3,9 +3,11 @@
 GTAR=go1.3.3.linux-amd64.tar.gz
 
 echo ">>> Installing Go..."
+pushd `pwd`
 cd $HOME
 [ -f $GTAR ]    || wget -q http://golang.org/dl/$GTAR
 [ -d $HOME/go ] || tar -xvpf $GTAR
+popd
 
 export GOROOT=/home/ubuntu/go
 export PATH=$GOROOT/bin:/home/ubuntu/bin:$PATH
