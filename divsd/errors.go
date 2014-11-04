8f31b414af9c9948dd8652834971b5d6ada2f714
Alvaro Saurin <alvaro.saurin@midokura.com>
2014-11-04 21:52:21 +0100

    A perrs timeout error

--- divsd/errors.go
+++ divsd/errors.go
@@ -12,3 +12,6 @@ var ERR_COULD_NOT_PARSE_ADDR = fmt.Errorf("Could not parse address")
 
 // Malformed message
 var ERR_MALFORMED_MSG = fmt.Errorf("Malformed message")
+
+// Timeout while waiting for peers
+var ERR_TIMEOUT_PEERS = fmt.Errorf("Timeout while waiting for peers")
